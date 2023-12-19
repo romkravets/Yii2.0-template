3057bd0e2618a2e2d6fb07505a92993de2e6ede2
romkravets <romann.kravets@gmail.com>
2023-12-19 20:07:49 +0200

    add project template

---
+++ views/site/index.php
@@ -0,0 +1,40 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Привіт!</h1>
+
+        <p class="lead">Мій перший проєкт</p>
+
+        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Почати</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4 mb-3">
+                <h2>XAMPP</h2>
+                <p>XAMPP — безкоштовна багатоплатформова збірка вебсервера з відкритим початковим кодом, що містить HTTP-сервер Apache, базу даних MariaDB, MySQL й інтерпретатори скриптів для мов програмування PHP та Perl, а також додаткові бібліотеки, що дозволяють запустити повноцінний вебсервер.</p>
+                <p><a class="btn btn-outline-secondary" href="https://www.apachefriends.org/index.html">XAMPP Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4 mb-3">
+                <h2>COMPOSER</h2>
+                <p>Composer — менеджер пакетів прикладного рівня для мови програмування PHP що забезпечує стандартний формат для управління залежностями у програмному забезпеченні та необхідними бібліотеками. Він був розроблений Нілом Адерманом і Хорді Боггіано, які і досі супроводжують проект.</p>
+                <p><a class="btn btn-outline-secondary" href="https://getcomposer.org/">Composer Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Yii 2.0</h2>
+
+                <p>Yii — це високопродуктивний вебфреймворк, написаний на PHP, реалізує парадигму модель-вид-контролер. Yii — скорочення від «Yes It Is!»</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/">Yii Documentation &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
